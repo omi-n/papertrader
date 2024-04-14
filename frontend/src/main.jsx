46a9fbfbcd5f52e1557b17a88b9e850f29e9ba14
Justin <justinsalamon17@gmail.com>
2024-04-14 16:41:08 -0400

    Added Sidebar w/ routing

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import Login from './pages/Login.jsx'
 import './index.css'
 import Register from './pages/Register.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from "./routes/root";
+import ErrorPage from "./pages/ErrorPage.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "Login",
+        element: <Login />,
+      },
+      {
+        path: "Sign Up",
+        element: <Register />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Login />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
