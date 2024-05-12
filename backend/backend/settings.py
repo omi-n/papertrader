9277c09b27451c31360014eee8f78adbd5ff54a5
Nabil Omi <nabilomi1@gmail.com>
2024-05-12 19:04:34 -0400

    glue for user buysell

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -140,6 +140,6 @@ REST_FRAMEWORK = {
 CORS_ALLOW_ALL_ORIGINS = True
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(months=1),
-    "REFRESH_TOKEN_LIFETIME": timedelta(months=3),
+    "ACCESS_TOKEN_LIFETIME": timedelta(weeks=4),
+    "REFRESH_TOKEN_LIFETIME": timedelta(weeks=12),
 }
---
+++ glue/user_utils.js
@@ -0,0 +1,107 @@
+import { get_access_token } from "./auth_utils.js";
+
+const GLOBAL_BASE_URL = "http://localhost:8000";
+
+async function get_balance() {
+    const access_token = get_access_token();
+    const response = await fetch(`${GLOBAL_BASE_URL}/api/user/balance/`, {
+        method: "GET",
+        headers: {
+            Authorization: `${access_token}`,
+        },
+    });
+    if (!response.ok) {
+        return { error: response };
+    }
+    const data = await response.json();
+    return data;
+}
+
+async function set_balance(amount) {
+    const access_token = get_access_token();
+    const response = await fetch(`${GLOBAL_BASE_URL}/api/user/balance/`, {
+        method: "POST",
+        headers: {
+            Authorization: `${access_token}`,
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ amount: amount }),
+    });
+    if (!response.ok) {
+        return { error: response };
+    }
+    const data = await response.json();
+    return data;
+}
+
+async function get_transactions() {
+    const access_token = get_access_token();
+    const response = await fetch(`${GLOBAL_BASE_URL}/api/user/transaction/`, {
+        method: "GET",
+        headers: {
+            Authorization: `${access_token}`,
+        },
+    });
+    if (!response.ok) {
+        return { error: response };
+    }
+    const data = await response.json();
+    return data;
+}
+
+async function get_stocks() {
+    const access_token = get_access_token();
+    const response = await fetch(`${GLOBAL_BASE_URL}/api/user/stock/`, {
+        method: "GET",
+        headers: {
+            Authorization: `${access_token}`,
+        },
+    });
+    if (!response.ok) {
+        return { error: response };
+    }
+    const data = await response.json();
+    return data;
+}
+
+async function buy_stock(ticker, quantity, price) {
+    const access_token = get_access_token();
+    const response = await fetch(`${GLOBAL_BASE_URL}/api/user/stock/`, {
+        method: "POST",
+        headers: {
+            Authorization: `${access_token}`,
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            ticker: ticker,
+            quantity: quantity,
+            price: price,
+        }),
+    });
+    if (!response.ok) {
+        return { error: response };
+    }
+    const data = await response.json();
+    return data;
+}
+
+async function sell_stock(ticker, quantity, price) {
+    const access_token = get_access_token();
+    const response = await fetch(`${GLOBAL_BASE_URL}/api/user/stock/`, {
+        method: "DELETE",
+        headers: {
+            Authorization: `${access_token}`,
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            ticker: ticker,
+            quantity: -quantity,
+            price: price,
+        }),
+    });
+    if (!response.ok) {
+        return { error: response };
+    }
+    const data = await response.json();
+    return data;
+}
