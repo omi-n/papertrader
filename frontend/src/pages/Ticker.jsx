c082c0b85fa34ba1e2f89fa21d40c402058c015b
Justin <justinsalamon17@gmail.com>
2024-05-17 00:01:46 -0400

    minor edit

--- frontend/src/pages/Ticker.jsx
+++ frontend/src/pages/Ticker.jsx
@@ -100,7 +100,7 @@ function Ticker() {
 
     return (
         <div>
-            <h1>Tickers</h1>
+            <h1>Stocks</h1>
             <div>
                 <input type="text" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} placeholder="Enter ticker symbol" />
                 <button onClick={handleSearch}>Search</button>
